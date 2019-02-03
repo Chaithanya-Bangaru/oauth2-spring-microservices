dc64fc0c2825fe058b68d8470cc52c21f4cc6c55
Chaithanya <chaithanya.bangaru@gmail.com>
2019-02-03 01:43:36 -0600

    updates

--- src/main/java/tech/bangaru/spring/microservices/security/AuthorizationServerConfig.java
+++ src/main/java/tech/bangaru/spring/microservices/security/AuthorizationServerConfig.java
@@ -1,28 +1,40 @@
 package tech.bangaru.spring.microservices.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
-public class AuthServerConfig implements AuthorizationServerConfigurer {
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig implements AuthorizationServerConfigurer {
 
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		// TODO Auto-generated method stub
+		// Identifying the trusted client
+		clients.inMemory().withClient("client")
+		.secret("{noop}secret")
+		.authorizedGrantTypes("password")
+		.resourceIds("oauth2-resources")
+		.scopes("read");
 
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		// TODO Auto-generated method stub
-
+		// Securing API end points
+		endpoints.authenticationManager(authenticationManager); 
 	}
 
 }
